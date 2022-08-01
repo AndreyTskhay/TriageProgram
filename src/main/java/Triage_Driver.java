310150b9902e32728f7e68f2fedf958ecb564fed
AndreyTskhay <tskhayandrey13@gmail.com>
2022-08-01 12:09:37 -0500

    Fixed some strings in the driver class and removed some redundant methods

--- src/main/java/Triage_Driver.java
+++ src/main/java/Triage_Driver.java
@@ -13,15 +13,15 @@ public class Triage_Driver {
 		
 		//each branch of the tree either leads to the answer or leads to another questions
 		
-		Position<String> root = triage.addRoot("Can he/she walk? (Yes/No)");
-		Position<String> breathing = triage.addLeft(root, "Is he/she breathing? (Yes/No)");
+		Position<String> root = triage.addRoot("Can the patient walk? (Yes/No)");
+		Position<String> breathing = triage.addLeft(root, "Can they breath? (Yes/No)");
 		triage.addRight(root, "Delayed care");
 		
-		Position<String> breathingAirway = triage.addLeft(breathing, "Open patient's airway. Is patient breathing? (Yes/No)");
+		Position<String> breathingAirway = triage.addLeft(breathing, "Open the patient's airway. Can they breath breathing? (Yes/No)");
 		triage.addLeft(breathingAirway, "Unsalvageable");
 		triage.addRight(breathingAirway, "Immediate Care");
 		
-		Position<String> countRespirations = triage.addRight(breathing, "Is count more or equal to 30?");
+		Position<String> countRespirations = triage.addRight(breathing, "Is the count greater or equal to 30?");
 		triage.addLeft(countRespirations, "Immediate care");
 		Position<String> radialPulse = triage.addRight(countRespirations, "is radial pulse present? (Yes/No)");
 		
@@ -32,18 +32,18 @@ public class Triage_Driver {
 		
 		//preparing input from a user
 		Scanner input = new Scanner(System.in);
-		String inputString = "";
+		String inputString;
 		Position<String> current = triage.root();
 		System.out.println(current.getElement());
 		
-		//accept input until the program gives an ultimate answer
+		//accept input until the program gives a final answer
 		do {
 			inputString = input.next();
-			if(inputString.trim().toLowerCase().equals("yes")) {
+			if(inputString.trim().equalsIgnoreCase("yes")) {
 				current = triage.right(current);
 				System.out.println(current.getElement());
 			}
-			else if(inputString.trim().toLowerCase().equals("no")) {
+			else if(inputString.trim().equalsIgnoreCase("no")) {
 				current = triage.left(current);
 				System.out.println(current.getElement());
 			}
